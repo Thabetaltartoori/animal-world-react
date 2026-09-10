34b64f6b4f5c4058cdbe0e8f7574e97bd2bf4a0f
Thabetaltartoori <thabetaltartoori@users.noreply.github.com>
2026-09-10 14:58:21 +0300

    remove unrelated files

--- data sciene/ds.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import pandas as pd
-def load_data(file_path):
-    return pd.read_excel(file_path)
--- data sciene/import numpy as np.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import time
-arr = np.array((1,2,3,4))
-print(arr)
-print(arr.dtype)
-print(arr.size)
-print(arr.ndim)
